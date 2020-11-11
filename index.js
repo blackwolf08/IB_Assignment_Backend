f76da75460e78669dd5ce65fbbe7ae81de6fdaf8
Sunny Dhama <sunnydhama@yahoo.com>
2020-11-11 16:01:07 +0530

    added newinterview route, validate user logic

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const connectDB = require('./database/index.js');
+const cors = require('cors');
+const multer = require('multer');
 const app = express();
+const appRoutes = require('./routes/index.js');
 
 const PORT = process.env.PORT || 5000;
+app.use(cors());
 
-const mysqlConnection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-});
+app.use(multer().array());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-connectDB.connect(mysqlConnection);
+app.use('/', appRoutes);
 
-//Importing Routes
-require('./routes/index.js')(app, mysqlConnection);
-
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log('Server Started!');
   console.log('Listening on PORT ', PORT);
-});
-
-process.on('SIGINT', () => {
-  console.log('\nServer Turn off Sequence Initiated!');
-  console.log('Disconnecting from Database');
-  connectDB.disconnect(mysqlConnection);
-  console.log('Closing Server!');
-  server.close();
-  console.log('Turn Off Sequence Complete! Exiting');
-  process.exit(1);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,95 @@
 const util = require('util');
+const express = require('express');
+const mysql = require('mysql');
+const moment = require('moment');
+const router = express.Router();
 
-module.exports = (app, mysqlConnection) => {
-  // OP in the chat
-  const query = util.promisify(mysqlConnection.query).bind(mysqlConnection);
+const connectDB = require('../database/index.js');
+const mysqlConnection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+});
+connectDB.connect(mysqlConnection);
 
-  app.get('/getInterviews', async (req, res) => {
-    let data = await query('select * from Interviews');
-    res.json(data);
+const query = util.promisify(mysqlConnection.query).bind(mysqlConnection);
+
+router.get('/getInterviews', async (req, res) => {
+  let interviewsList = await query('select * from Interviews');
+  let { date } = req.query;
+  let currentInterviewList = [];
+  interviewsList.forEach((interview) => {
+    let curr_date = new Date(parseInt(interview.start_time))
+      .toISOString()
+      .split('T')[0];
+    if (curr_date == date) {
+      currentInterviewList.push(interview);
+    }
   });
-};
+  if (currentInterviewList[0] == null) res.json([]);
+  res.json(currentInterviewList);
+});
+
+router.get('/is-user-available', async (req, res) => {
+  let { start_time, end_time, email, user_role } = req.query;
+  let user = await query(`select * from User where email="${email}"`);
+  if (user[0]?.user_role !== user_role || user?.length == 0) {
+    res.json({
+      status: 'err',
+    });
+  }
+  start_time = parseInt(start_time);
+  end_time = parseInt(end_time);
+  let userInterviewsList = await query(
+    `select * from Interviews where ${user_role}="${email}"`
+  );
+
+  userInterviewsList.map((interview) => {
+    let interview_start_time = parseInt(interview.start_time);
+    let interview_end_time = parseInt(interview.end_time);
+
+    if (start_time <= interview_start_time && end_time >= interview_end_time) {
+      res.json({
+        status: 'err',
+      });
+    } else if (
+      start_time >= interview_start_time &&
+      end_time <= interview_end_time
+    ) {
+      res.json({
+        status: 'err',
+      });
+    } else if (
+      start_time <= interview_start_time &&
+      end_time <= interview_end_time &&
+      end_time >= interview_start_time
+    ) {
+      res.json({
+        status: 'err',
+      });
+    } else if (
+      start_time >= interview_start_time &&
+      start_time <= interview_end_time &&
+      end_time >= interview_end_time
+    ) {
+      res.json({
+        status: 'err',
+      });
+    }
+  });
+
+  res.json({
+    status: 'ok',
+  });
+});
+
+router.post('/addInterview', async (req, res) => {
+  let { start_time, end_time, duration, interviewee, interviewer } = req.body;
+  await query(
+    `insert into Interviews(start_time, end_time, duration, interviewee, interviewer) values("${start_time}", "${end_time}", "${duration}", "${interviewee}", "${interviewer}")`
+  );
+  res.send(200);
+});
+
+module.exports = router;
