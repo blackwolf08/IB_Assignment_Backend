ec17d9c65ac4b37b7c122916be10c245ce225bae
Sunny Dhama <sunnydhama@yahoo.com>
2020-11-11 17:15:23 +0530

    added cron job logic for email scheduling

---
+++ handlers/EmailScheduler.js
@@ -0,0 +1,38 @@
+const nodemailer = require('nodemailer');
+const moment = require('moment');
+const cron = require('node-cron');
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: `dhamasunny98@gmail.com`,
+    pass: `wulvclixidhsvwmh`,
+  },
+});
+
+let mailOptions = {
+  from: 'dhamasunny98@gmail.com',
+  subject: 'You have interview scheduled for IB!',
+};
+
+const scheduleEmail = (invitee, time) => {
+  let scheduleCronString = `${moment(time)
+    .subtract(10, 'minutes')
+    .format('m H DD M')} *`;
+  console.log(scheduleCronString);
+  cron.schedule(scheduleCronString, () => {
+    mailOptions['to'] = invitee;
+    mailOptions['text'] = `You have your email scheduled for ${time}`;
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  });
+};
+
+module.exports = {
+  scheduleEmail,
+};
